4e4619c4ea0eee1d812895fde4176a87d48ae92e
Eyewavu <ew.music.yt@gmail.com>
2022-12-21 20:13:16 +0100

    lil update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,34 @@
 /** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
-  env: { browser: false },
+  env: {
+    browser: false,
+    node: true
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "ES2019",
+    ecmaVersion: "latest",
     sourceType: "module"
   },
   plugins: [
     "@typescript-eslint",
   ],
-  rules: {
+rules: {
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/ban-tslint-comment":"error",
+    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/consistent-type-definitions": ["warn","type"],
     "arrow-spacing":["warn"],
-    "camelcase":["warn"],
-    "capitalized-comments":["warn"],
     "consistent-return":["error"],
     "default-case-last":["error"],
     "default-case":["warn"],
     "dot-notation":["error"],
     "eqeqeq":["error"],
     "linebreak-style": ["error","unix"],
-    "no-console":["warn"],
     "no-constant-binary-expression":["error"],
     "no-duplicate-imports":["warn",{"includeExports":true}],
     "no-empty-function":["error"],
@@ -31,12 +36,10 @@ module.exports = {
     "no-floating-decimal":["warn"],
     "no-lone-blocks":["error"],
     "no-lonely-if":["error"],
-    "no-mixed-operators":["error"],
     "no-new-wrappers":["error"],
     "no-self-compare":["error"],
     "no-unreachable-loop":["error"],
     indent: ["error",2],
     quotes: ["error","double"],
     semi: ["warn","never"],
-  }
-}
+  }}
